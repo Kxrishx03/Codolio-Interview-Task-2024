2b56d2eedfc41c05fd4fb3476f759649f11ccf8b
Kxrishx03 <2001karishmamandal@gmail.com>
2024-07-21 23:47:00 +0530

    "DONE"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { Home } from "./pages/Home";
 import { Navbar } from "./components/Navbar";
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import './App.css';
 
 export default function App() {
   const lightTheme = useSelector((state) => state.themeKey);
 
-  return (
+  console.log('Current theme:', lightTheme);
 
+  return (
     <div className={`${lightTheme ? 'bg-slate-50 text-black' : 'bg-gray-800 text-white'}`}>
         <Navbar />
         <Home />
     </div>
-  )
+  );
 }
