10c7dcd6b4e9ecccbe1c7130d692e65f8fbf94b3
Kxrishx03 <2001karishmamandal@gmail.com>
2024-07-22 00:03:36 +0530

    ""

--- Frontend/src/components/SearchBar.jsx
+++ Frontend/src/components/SearchBar.jsx
@@ -20,7 +20,7 @@ export function SearchBar({ filtervalues, setFilterValues, applyFilter, resetFil
     return (
         <div className={`flex search justify-evenly items-center w-10/12 lg:w-8/12 rounded mt-4 ${lightTheme ? 'bg-neutral-200 text-black' : 'bg-gray-800 text-white'}`}>
             <div className='s-left flex w-full justify-center'>
-                <div className={`flex items-center w-8/12 rounded border ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'} p-2`}>
+                <div className={`flex items-center w-8/12 rounded border-none ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'} p-2`}>
                     <input
                         onChange={(e) => setData(e.target.value)}
                         type="text"
@@ -29,16 +29,16 @@ export function SearchBar({ filtervalues, setFilterValues, applyFilter, resetFil
                     />
                     <button onClick={applyFilter}><SearchIcon /></button>
                 </div>
-                <button onClick={applyFilter} className='h-10 w-2/12 bg-blue-500 mx-1 px-1 rounded'>Apply</button>
-                <button onClick={resetFilter} className='h-10 w-2/12 bg-blue-500 mx-1 px-1 rounded'>Reset</button>
+                <button onClick={applyFilter} className='h-10 w-2/12 bg-blue-500 mx-1 px-1 rounded border-none'>Apply</button>
+                <button onClick={resetFilter} className='h-10 w-2/12 bg-blue-500 mx-1 px-1 rounded border-none'>Reset</button>
             </div>
             <div className='s-right flex w-full justify-center'>
-                <select onChange={(e) => handleFilter(e)} value={filtervalues.type} name='type' className={`p-2 f-type rounded border w-4/12 ml-2 ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'}`}>
+                <select onChange={(e) => handleFilter(e)} value={filtervalues.type} name='type' className={`p-2 f-type rounded border-none w-4/12 ml-1 ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'}`}>
                     <option value="">Type</option>
                     <option value="income">Income</option>
                     <option value="expense">Expense</option>
                 </select>
-                <select onChange={(e) => handleFilter(e)} value={filtervalues.category} name='category' className={`p-2 rounded border w-4/12 ml-2 filter-s ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'}`}>
+                <select onChange={(e) => handleFilter(e)} value={filtervalues.category} name='category' className={`p-2 rounded border-none w-4/12 ml-2 filter-s ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'}`}>
                     <option value="">Category</option>
                     <option value="Healthcare">Healthcare</option>
                     <option value="Shopping">Shopping</option>
@@ -49,7 +49,7 @@ export function SearchBar({ filtervalues, setFilterValues, applyFilter, resetFil
                     <option value="Education">Education</option>
                     <option value="Transportation">Transportation</option>
                 </select>
-                <select onChange={(e) => handleFilter(e)} value={filtervalues.currency} name='currency' className={`p-2 rounded border w-4/12 ml-2 filter-s ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'}`}>
+                <select onChange={(e) => handleFilter(e)} value={filtervalues.currency} name='currency' className={`p-2 rounded border-none  w-4/12 ml-2 filter-s ${lightTheme ? 'bg-white text-black' : 'bg-gray-700 text-white'}`}>
                     <option value="">Currency</option>
                     <option value="USD">USD</option>
                     <option value="INR">INR</option>
